66bf73f39c827c93884ba46a665fabc59b0b9545
jihansoft <jihanuddinsoft@gmail.com>
2025-05-25 21:29:05 +0600

    update form responsive issue fix

--- src/common/FormPop.tsx
+++ src/common/FormPop.tsx
@@ -77,7 +77,7 @@ export default function FormPop({ onClose }: Props) {
     }
   };
   return (
-    <div className="fixed inset-0 bg-[#17102FBA] bg-opacity-30 flex items-start justify-center z-50 p-2 ">
+    <div className="fixed inset-0 bg-[#17102FBA] bg-opacity-30 flex items-start justify-center z-50 lg:p-2 md:p-4 sm:p-6 p-6  overflow-y-auto ">
       {/* Close Button */}
       <button
         onClick={onClose}
